4d3f05ec7b9d0e42f677dfe9a0653b74799a7f58
spark131072 <spark131072@gmail.com>
2018-09-28 11:58:47 +0800

    280918_week3_05

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,9 +1,27 @@
+"""
+131072
+280918
+CS+X
+"""
+
+
+
+
+
+
+# =============
+
 from django.shortcuts import render,redirect   # 加入 redirect 套件
 from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+import random
 
+
+
+# =============
 
 def index(request):
-	return render(request, 'spark_syz.html')
+    Kort = str("Spark")
+    return render(request, 'spark_syz.html')
