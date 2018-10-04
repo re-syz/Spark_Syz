00e51c11890df80efee2813f5fd1cc72120dcab2
spark131072 <spark131072@gmail.com>
2018-10-04 23:51:52 +0800

    041018_week3_10

--- helloworld/views.py
+++ helloworld/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
 import random
 
 
@@ -25,8 +26,13 @@ import random
 def index(request):
     Kort = "Kort"
     Spark = "Spark"
+
+    imgids = [random.randint(771,791) for i in range(20)]
+    imgurls_local = ["DSC_0{}".format(imgid_local) for imgid_local in imgids_local]
+
     default_msg = "Hey! Here are random photos on the Internet."
     msgs = [default_msg for i in range(len(default_msg))]
     imgids = [random.randint(1, 1072) for i in range(len(default_msg))]
     imgurls = ["https://picsum.photos/200/200/?image={}".format(imgid) for imgid in imgids]
+
     return render(request, 'index.html', locals())
