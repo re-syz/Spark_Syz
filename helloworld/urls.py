b161fcfe8f5e767f0a6b81ae8e35c9bdd8411cff
spark131072 <spark131072@gmail.com>
2018-10-12 10:14:36 +0800

    121018_week5_02

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -1,18 +1,28 @@
-"""helloworld URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """helloworld URL Configuration
+#
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/2.1/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
 """
+131072
+121018
+urls.py
+"""
+
+
+
+# =============
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
@@ -22,7 +32,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
-    path('about.html/', views.about, name = 'about'),
-    path('contact.html/', views.contact, name = 'contact'),
-    path('portfolio.html/', views.portfolio, name = 'portfolio')
+    path('about/', views.about, name = 'about'),
+    path('contact/', views.contact, name = 'contact'),
+    path('portfolio/', views.portfolio, name = 'portfolio')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
